b528c5b204d60c96d281a5a8ca169b895a47b7c4
obinna-okoro1 <edgeron.ng@gmail.com>
2025-08-11 20:52:31 +0000

    anonkey ion main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
-(window as any).process = {
-  env: {
-    SUPABASE_URL: 'https://unmnypzveqgbfzeshouc.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVubW55cHp2ZXFnYmZ6ZXNob3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MjMwMDYsImV4cCI6MjA2ODM5OTAwNn0.ECt5lT6o71msFIV1irzHdohobD03JLNGVvDuBzLkDSI'
-  }
-};
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
