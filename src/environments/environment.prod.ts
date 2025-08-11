a381d053f1e42f5f03e536aaa92394f0643bbb89
obinna-okoro1 <edgeron.ng@gmail.com>
2025-08-11 20:20:29 +0000

    supabasekey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: process.env['SUPABASE_URL'] || '',
-  supabaseKey: process.env['SUPABASE_KEY'] || '',
+  supabaseUrl: process.env['SUPABASE_URL'] || 'https://unmnypzveqgbfzeshouc.supabase.co',
+  supabaseKey: process.env['SUPABASE_KEY'] || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVubW55cHp2ZXFnYmZ6ZXNob3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MjMwMDYsImV4cCI6MjA2ODM5OTAwNn0.ECt5lT6o71msFIV1irzHdohobD03JLNGVvDuBzLkDSI',
 };
