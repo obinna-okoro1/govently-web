bb958a1f5d0236128aea8a2ae7fdb0465971a87e
obinna-okoro1 <edgeron.ng@gmail.com>
2025-08-15 15:57:16 +0000

    supabase publishable key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://unmnypzveqgbfzeshouc.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVubW55cHp2ZXFnYmZ6ZXNob3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MjMwMDYsImV4cCI6MjA2ODM5OTAwNn0.ECt5lT6o71msFIV1irzHdohobD03JLNGVvDuBzLkDSI'
+  supabaseKey: 'sb_publishable_7DmY1bygdN_NBJz48U4rFw_gSzdoqdi'
 };
